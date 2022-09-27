877c40d1b57c7152b1a70eb0dfd7d80e573e18d1
David Morales <davidbate5025@gmail.com>
2022-09-27 11:42:38 -0500

    arreglo de rutas

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express = require('express')
+const express = require("express");
 //const home = require('./categories/home')
-const path = require('path')
-const fs = require('fs')
+const path = require("path");
+const fs = require("fs");
 
-
-
-const app = express()
-app.use(express.static(__dirname + '/public'));
+const app = express();
+app.use(express.static(__dirname + "/public"));
 
 /*
 //Creating files
@@ -31,96 +29,77 @@ fs.readFile(__dirname + '/api/categories/home', 'utf8', function (err, data) {
 });
 */
 
-
 //app.use('/static', express.static('api'));
 //app.use('/api', express.static(path.join(__dirname, 'api')))
-
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({
 //         ok:true,
-        
+
 //     })
 // });
 
-app.get('/', function(req, res) {
-
-    
-    res.json({
-        hola: 'holaa'
-    })
-
+app.get("/", function (req, res) {
+  res.json({
+    hola: "holaa",
+  });
 });
 
-app.get('/products/home_', function(req, res) {
-
-    
-    serveFile(res, 'api/products/home_');
-
+app.get("/products/home_", function (req, res) {
+  serveFile(res, "api/products/home_");
 });
 
-app.get('/products/list/for-category/:id', function(req, res) {
-    const {id} = req.params
-    
-    serveFile(res, `api/products/list/for-category/${id}`);
+app.get("/products/list/for-category/:id", function (req, res) {
+  const { id } = req.params;
 
+  serveFile(res, `api/products/list/for-category/${id}`);
 });
 
-app.get('/products/home/for-category/:id', function(req, res) {
-    const {id} = req.params
-    serveFile(res, `api/products/home/for-category/${id}`);
+app.get("/products/home/for-category/:id", function (req, res) {
+  const { id } = req.params;
+  serveFile(res, `api/products/home/for-category/${id}`);
 });
 
-app.get('/products/detail/:id', function(req, res) {
-    const {id} = req.params;    
-    serveFile(res, `api/products/detail/${id}.json`);
-
+app.get("/products/detail/:id", function (req, res) {
+  const { id } = req.params;
+  serveFile(res, `api/products/detail/${id}.json`);
 });
 
-app.get('/users/me', function(req, res) {
-
-    
-    serveFile(res, 'api/users/me');
-
+app.get("/users/me", function (req, res) {
+  serveFile(res, "api/users/me");
 });
 
-app.get('/categories/home', function(req, res) {
-
-    
-    serveFile(res, 'api/categories/home');
-
+app.get("/categories/home", function (req, res) {
+  serveFile(res, "api/categories/home");
 });
 
-app.get('/chats/user/:id', function(req, res) {
-    const {id} = req.params
-    
-    serveFile(res, `api/chats/user/${id}`);
+app.get("/chats/user/:id", function (req, res) {
+  const { id } = req.params;
 
+  serveFile(res, `api/chats/user/${id}`);
 });
 
-app.get('/categories/:id', function(req, res) {
-    const {id} = req.params
-    
-    serveFile(res, `api/categories/${id}`);
+app.get("/categories/:id", function (req, res) {
+  const { id } = req.params;
 
+  serveFile(res, `api/categories/${id}`);
 });
 
 function serveFile(res, pathName, mime) {
-    
-    mime = mime || 'application/json';
-    const basepath = __dirname.replace('/./', '')
-    
-    fs.readFile(basepath + '/' + pathName, function (err, data) {
-        if (err) {
-            res.writeHead(500, {"Content-Type": "text/plain"});
-            return res.end('Error loading ' + pathName + " with Error: " + err);
-        }
-        res.writeHead(200, {"Content-Type": mime});
-        res.end(data);
-    });
-    
+  mime = mime || "application/json";
+  const file = path.join(process.cwd(), "", pathName);
+
+  fs.readFile(file, function (err, data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      return res.end("Error loading " + pathName + " with Error: " + err);
+    }
+    res.writeHead(200, { "Content-Type": mime });
+    res.end(data);
+  });
 }
 
-app.listen(8000, ()=>{
-    console.log('el servidor se esta corriendo')
-})
+
+app.listen(8000, () => {
+  console.log("el servidor se esta corriendo");
+});
